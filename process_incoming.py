ae6992071eb66ef12f55135cf04cafbd73df2b4b
AyushGup11 <ayushgupta2131016@gmail.com>
2026-03-15 17:32:13 +0530

    LLM model added and name changed

--- process_incoming.py
+++ process_incoming.py
@@ -39,6 +39,18 @@ def create_embedding(text_list):
     
     return all_embeddings
 
+def inference(prompt):
+    r = requests.post("http://localhost:11434/api/generate", json={
+        # "model": "deepseek-r1",
+        "model": "llama3.2",
+        "prompt": prompt,
+        "stream": False
+    })
+
+    response = r.json()
+    print(response)
+    return response
+
 df = joblib.load('embeddings.joblib')
 
 incoming_query = input("Ask a Question: ")
@@ -52,5 +64,22 @@ mac_indx = similarities.argsort()[::-1][0:top_results]
 new_df = df.loc[mac_indx]
 #print(new_df[["title", "number", "text"]])
 
-for index, item in new_df.iterrows():
-    print(index, item["title"], item["number"], item["text"], item["start"], item["end"])
+prompt = f'''I am teaching web development in my Sigma web development course. Here are video subtitle chunks containing video title, video number, start time in seconds, end time in seconds, the text at that time:
+
+{new_df[["title", "number", "start", "end", "text"]].to_json(orient="records")}
+---------------------------------
+"{incoming_query}"
+User asked this question related to the video chunks, you have to answer in a human way (dont mention the above format, its just for you) where and how much content is taught in which video (in which video and at what timestamp) and guide the user to go to that particular video. If user asks unrelated question, tell him that you can only answer questions related to the course
+'''
+with open("prompt.txt", "w") as f:
+    f.write(prompt)
+
+response = inference(prompt)["response"]
+print(response)
+
+with open("response.txt", "w") as f:
+    f.write(response)
+
+
+#for index, item in new_df.iterrows():
+ #   print(index, item["title"], item["number"], item["text"], item["start"], item["end"])
